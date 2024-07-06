0a910b8755409ce695fca195974c9125727a0975
nix.dev <nixcrownzz@gmail.com>
2024-07-06 15:33:02 +0200

    Bot online, receiving messages.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const { token } = require('./config.json');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json');
 
+// Discord client
+const client = new Client({ intents: Object.values(GatewayIntentBits) });
+
+// On client connection
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Server online (@${readyClient.user.username}).`);
+});
+
+// On client message reception
+client.on(Events.MessageCreate, async (message) => {
+    if(!message.content.startsWith(config.prefix)) return;
+    console.log(`${message.author.username} : ${message.content}`);
+});
+
+// Discord client login
+client.login(config.token).catch(e => {
+    console.log(`Client could not login.`);
+});
